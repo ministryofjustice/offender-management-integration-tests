19ae71a0ead78ff4f70b68b48fb4db9472f02c3e
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-12-05 12:17:56 +0000

    Add test to check auth protected status call

--- spec/integration/fetch_status_spec.rb
+++ spec/integration/fetch_status_spec.rb
@@ -8,4 +8,20 @@ RSpec.feature 'Fetching the status' do
 
     expect(page).to have_content('Everything is fine.')
   end
+
+  scenario 'returns the database status from the allocation api' do
+    start_page = ENV.fetch('START_PAGE')
+
+    visit start_page
+
+    expect(page).to have_content('Username')
+    expect(page).to have_content('Password')
+
+    fill_in 'Username', with: ENV.fetch('STAFF_USERNAME')
+    fill_in 'Password', with: ENV.fetch('STAFF_PASSWORD')
+    click_button('Sign in')
+
+    expect(page).to have_css('.status', text: 'ok')
+    expect(page).to have_css('.postgres_version', text: 'PostgreSQL 10.5')
+  end
 end
