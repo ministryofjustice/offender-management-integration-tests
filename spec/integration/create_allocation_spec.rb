751b530b37f958717d6ef6864a568d105fce22c0
Toby Retallick <toby.retallick@gmail.com>
2019-02-15 11:53:58 +0000

    Create case information for a prisoner before assigning a POM

--- spec/integration/create_allocation_spec.rb
+++ spec/integration/create_allocation_spec.rb
@@ -5,8 +5,17 @@ require 'spec_helper'
 RSpec.feature 'Allocation' do
   scenario 'create an allocation' do
     sign_in_and_go ENV.fetch('START_PAGE')
-    click_link 'Awaiting allocation'
+    click_link 'Awaiting information'
 
+    within('.offender_row_0') do
+      click_link 'Edit'
+    end
+
+    find('#case_information_case_allocation_NPS', visible: false).choose
+    find('#case_information_tier_A', visible: false).choose
+    click_button 'Save'
+
+    visit "#{ENV.fetch('START_PAGE')}/allocations#awaiting-allocation"
     within('.offender_row_0') do
       click_link 'Allocate'
     end
