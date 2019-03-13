6160de4741876a950c2f0ccec176c0877ff6d97b
Toby Retallick <toby.retallick@gmail.com>
2019-03-13 14:44:52 +0000

    Rename START_PAGE env var

--- spec/integration/fetch_status_spec.rb
+++ spec/integration/fetch_status_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.feature 'Check health endpoint' do
   scenario 'returns a status message from the health endpoint' do
-    start_page = ENV.fetch('START_PAGE') + '/health'
+    start_page = ENV.fetch('STAGING_START_PAGE') + '/health'
     visit start_page
 
     expect(page).to have_content('Everything is fine.')
